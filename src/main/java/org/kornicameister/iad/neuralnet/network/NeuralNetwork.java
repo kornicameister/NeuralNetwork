a284fc23f8d711818ccc6caa02eb970b3b707e2a
kornicameister <kornicameister@gmail.com>
2013-03-17 20:50:50 +0100

    method to operate upon layers in network

--- src/main/java/org/kornicameister/iad/neuralnet/network/NeuralNetwork.java
+++ src/main/java/org/kornicameister/iad/neuralnet/network/NeuralNetwork.java
@@ -5,6 +5,7 @@ import org.kornicameister.iad.neuralnet.NeuralProcessable;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * NeuralNetwork represents network built from
@@ -18,10 +19,21 @@ import java.util.List;
 public class NeuralNetwork implements NeuralProcessable {
     private final static Logger LOGGER = Logger.getLogger(NeuralNetwork.class);
     private List<NeuralLayer> layerList = new LinkedList<>();
+    private Double[] result;
 
+    public NeuralNetwork(int size) {
+        this.result = new Double[size];
+    }
+
+    /**
+     * Teaching in neural network performed in backward
+     * way...teaching from output to input.
+     */
     @Override
     public void teach() {
-        for (NeuralLayer layer : this.layerList) {
+        ListIterator<NeuralLayer> iterator = this.layerList.listIterator(this.layerList.size());
+        while (iterator.hasPrevious()) {
+            final NeuralLayer layer = iterator.previous();
             LOGGER.info(String.format("Teaching layer %s", layer));
             layer.teach();
         }
@@ -40,5 +52,29 @@ public class NeuralNetwork implements NeuralProcessable {
         for (NeuralLayer layer : this.layerList) {
             layer.initByRandom(lower, higher);
         }
+    }
+
+    public boolean addLayer(NeuralLayer neuralLayer) {
+        return layerList.add(neuralLayer);
+    }
+
+    public NeuralLayer removeLayer(int i) {
+        return layerList.remove(i);
+    }
+
+    public boolean removeLayer(Object o) {
+        return layerList.remove(o);
+    }
+
+    public NeuralLayer getLayer(int i) {
+        return layerList.get(i);
+    }
+
+    public List<NeuralLayer> getLayers() {
+        return layerList;
+    }
+
+    public void setLayers(List<NeuralLayer> layerList) {
+        this.layerList = layerList;
     }
 }
