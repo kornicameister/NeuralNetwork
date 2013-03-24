17547fe06b969383dea251559f41bf531147938d
kornicameister <kornicameister@gmail.com>
2013-03-24 21:40:36 +0100

    code refactored...readability increased...

--- src/main/java/org/kornicameister/iad/neuralnet/NeuralLayer.java
+++ src/main/java/org/kornicameister/iad/neuralnet/NeuralLayer.java
@@ -2,10 +2,9 @@ package org.kornicameister.iad.neuralnet;
 
 import org.apache.log4j.Logger;
 import org.kornicameister.iad.neuralnet.core.NeuralProcessable;
+import org.kornicameister.iad.neuralnet.impl._NeuralLayer;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.ListIterator;
 
 /**
@@ -16,9 +15,8 @@ import java.util.ListIterator;
  * @author kornicameister
  * @since 0.0.1
  */
-public class NeuralLayer implements NeuralProcessable {
+public class NeuralLayer extends _NeuralLayer implements NeuralProcessable {
     private final static Logger LOGGER = Logger.getLogger(NeuralLayer.class);
-    private List<Neuron> neurons = new LinkedList<>();
 
     public NeuralLayer() {
     }
@@ -50,34 +48,6 @@ public class NeuralLayer implements NeuralProcessable {
         for (Neuron neuron : this.neurons) {
             neuron.initByRandom(lower, higher);
         }
-    }
-
-    public boolean addNeuron(Neuron neuron) {
-        return this.neurons.add(neuron);
-    }
-
-    public Neuron getNeuron(int i) {
-        return this.neurons.get(i);
-    }
-
-    public Neuron removeNeuron(int i) {
-        return this.neurons.remove(i);
-    }
-
-    public boolean removeNeuron(Object o) {
-        return this.neurons.remove(o);
-    }
-
-    public List<Neuron> getNeurons() {
-        return this.neurons;
-    }
-
-    public void clearNeurons() {
-        this.neurons.clear();
-    }
-
-    public int getSize() {
-        return this.neurons.size();
     }
 
     @Override
