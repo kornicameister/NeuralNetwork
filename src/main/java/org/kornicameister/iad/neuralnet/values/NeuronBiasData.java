7a40c58e2467b48dc0f20992cd09cea0180fa2dd
Tomasz Trębski <kornicameister@gmail.com>
2013-06-29 19:13:45 +0200

    data for neuron (extracted from Neuron's previous version)

---
+++ src/main/java/org/kornicameister/iad/neuralnet/values/NeuronBiasData.java
@@ -0,0 +1,96 @@
+package org.kornicameister.iad.neuralnet.values;
+
+import org.kornicameister.iad.neuralnet.Neuron;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class NeuronBiasData extends NeuronData {
+    public static final Double BIAS_VALUE = 1.0;
+    private Double biasWeight;
+    private Double oldBiasWeight;
+
+    public NeuronBiasData(Double biasWeight, Double[] signals, Double[] weights) {
+        super(signals, weights);
+        this.biasWeight = biasWeight;
+        this.oldBiasWeight = biasWeight;
+    }
+
+    public Double getOldBiasWeight() {
+        return oldBiasWeight;
+    }
+
+    @Override
+    public Double[] getOldWeights() {
+        List<Double> oldWeights = Arrays.asList(super.getOldWeights().clone());
+        oldWeights.add(0, this.oldBiasWeight);
+        return oldWeights.toArray(new Double[oldWeights.size()]);
+    }
+
+    @Override
+    public void setOldWeights(Double[] oldWeights) {
+        List<Double> oldWeightsTmp = Arrays.asList(oldWeights.clone());
+        this.oldBiasWeight = oldWeightsTmp.remove(0);
+        super.setWeights(oldWeightsTmp.toArray(new Double[oldWeightsTmp.size()]));
+    }
+
+    @Override
+    public Double[] getSignals() {
+        List<Double> signals = Arrays.asList(super.getSignals().clone());
+        signals.add(0, BIAS_VALUE);
+        return signals.toArray(new Double[signals.size()]);
+    }
+
+    @Override
+    public void setSignals(Double[] signals) {
+        List<Double> signalsTmp = Arrays.asList(signals.clone());
+        super.setSignals(signalsTmp.toArray(new Double[signalsTmp.size()]));
+    }
+
+    @Override
+    public Double[] getWeights() {
+        List<Double> weights = Arrays.asList(super.getWeights().clone());
+        weights.add(0, this.biasWeight);
+        return weights.toArray(new Double[weights.size()]);
+    }
+
+    @Override
+    public void setWeights(Double[] weights) {
+        List<Double> weightsTmp = Arrays.asList(weights.clone());
+        this.biasWeight = weightsTmp.remove(0);
+        super.setWeights(weightsTmp.toArray(new Double[weightsTmp.size()]));
+    }
+
+    @Override
+    public Double getSignalAt(int index) {
+        if (index == 0) {
+            return BIAS_VALUE;
+        }
+        return super.getSignalAt(index - 1);
+    }
+
+    @Override
+    public Double getWeightAt(int index) {
+        if (index == 0) {
+            return this.getBiasWeight();
+        }
+        return super.getWeightAt(index - 1);
+    }
+
+    public Double getBiasWeight() {
+        return biasWeight;
+    }
+
+    @Override
+    public String toString() {
+        return "NeuronBiasData{" +
+                "biasWeight=" + biasWeight +
+                ", oldBiasWeight=" + oldBiasWeight +
+                "} " + super.toString();
+    }
+}
---
+++ src/main/java/org/kornicameister/iad/neuralnet/values/NeuronData.java
@@ -0,0 +1,88 @@
+package org.kornicameister.iad.neuralnet.values;
+
+import com.google.common.base.Preconditions;
+import org.kornicameister.iad.neuralnet.Neuron;
+import org.kornicameister.iad.neuralnet.core.NeuralData;
+
+import java.util.Arrays;
+
+/**
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class NeuronData implements NeuralData {
+    private Neuron neuron;
+    private Double[] oldWeights;
+    private Double[] weights;
+    private Double[] signals;
+
+    public NeuronData(Double[] signals, Double[] weights) {
+        this.signals = signals.clone();
+        this.weights = weights.clone();
+        this.oldWeights = weights.clone();
+    }
+
+    @Override
+    public Double[] getOldWeights() {
+        return oldWeights;
+    }
+
+    @Override
+    public void setOldWeights(Double[] oldWeights) {
+        this.oldWeights = oldWeights.clone();
+    }
+
+    @Override
+    public Double[] getSignals() {
+        return signals;
+    }
+
+    @Override
+    public void setSignals(Double[] signals) {
+        this.signals = signals;
+    }
+
+    @Override
+    public Double[] getWeights() {
+        return weights;
+    }
+
+    @Override
+    public void setWeights(Double[] weights) {
+        this.weights = weights.clone();
+    }
+
+    @Override
+    public final int getSize() {
+        return this.weights.length;
+    }
+
+    public Neuron getNeuron() {
+        return neuron;
+    }
+
+    public void setNeuron(final Neuron neuron) {
+        this.neuron = neuron;
+    }
+
+    public Double getSignalAt(final int index) {
+        Preconditions.checkArgument(index >= 0 && index <= (this.getSize() - 1), "Invalid signal index");
+        return this.signals[index];
+    }
+
+    public Double getWeightAt(final int index) {
+        Preconditions.checkArgument(index >= 0 && index <= (this.getSize() - 1), "Invalid weight index");
+        return this.weights[index];
+    }
+
+    @Override
+    public String toString() {
+        return "NeuronData{" +
+                "neuron=" + neuron +
+                ", oldWeights=" + Arrays.toString(oldWeights) +
+                ", weights=" + Arrays.toString(weights) +
+                ", signals=" + Arrays.toString(signals) +
+                "} " + super.toString();
+    }
+}
